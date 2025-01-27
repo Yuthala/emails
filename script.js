57018b94a4234c167255a2e6a31f4af847e3c487
Yuthala <dinapolousova@gmail.com>
2025-01-27 14:04:27 +0300

    timer

--- script.js
+++ script.js
@@ -1,23 +1,89 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-const second = 1000;
-const minute = 60 * second;
-const hour = 60 * minute;
-const day = 24 * hour;
+	const updateTimer = setInterval(function() {
+	future = Date.parse("Mar 31, 2025 23:59:59");
+	now = new Date();
+	diff = future - now;
+	
+	days = Math.floor(diff / (1000 * 60 * 60 * 24));
+	console.log(`days: ${days}`)
+	hours = Math.floor(diff / (1000 * 60 * 60));
+	console.log(`hours: ${hours}`)
+	mins = Math.floor(diff / (1000 * 60));
+	secs = Math.floor(diff / 1000);
+	
+	d = days;
+	let daysText;
+	if(days === 11 || days === 12) {
+		daysText = 'дней'
+	} else {
+		if(days % 10 === 1) {
+			daysText = 'день'
+		} else if(days % 10 === 2 || days % 10 === 3 || days % 10 === 4) {
+			daysText = 'дня'
+		} else {
+			daysText = 'дней'
+		}
+	}
 
-setInterval(() => {
-    let diff = Date.parse("Mar 31, 2025 23:59:59") - Date.now();
-    const days = Math.floor(diff / day);
-    diff -= days * day;
-    const hours = Math.floor(diff / hour);
-    diff -= hours * hour;
-    const mins = Math.floor(diff / minute);
-    diff -= mins * minute;
-    const secs = Math.floor(diff / second);
-    document.getElementById("timer").innerHTML = `
-        <div>${days}<span>Days</span></div>
-        <div>${hours}<span>Hours</span></div>
-        <div>${mins}<span>Minutes</span></div>
-        <div>${secs}<span>Seconds</span></div>`;
-}, 1000);
+	h = hours - days * 24;
+	let hoursText;
+	if(h === 11 || h === 12) {
+		hoursText = 'часов'
+	} else {
+		if(h % 10 === 1) {
+			hoursText = 'час'
+		} else if(h % 10 === 2 || h % 10 === 3 || h % 10 === 4) {
+			hoursText = 'часа'
+		} else {
+			hoursText = 'часов'
+		}
+	}
+
+	m = mins - hours * 60;
+	let minsText;
+	if(m === 11 || m === 12) {
+		minsText = 'минут'
+	} else {
+		if(m % 10 === 1) {
+			minsText = 'минута'
+		} else if(m % 10 === 2 || m % 10 === 3 || m % 10 === 4) {
+			minsText = 'минуты'
+		} else {
+			minsText = 'минут'
+		}
+	}
+
+	s = secs - mins * 60;
+	let secsText;
+	if(s === 11 || s === 12) {
+		secsText = 'секунд'
+	} else {
+		if(s % 10 === 1) {
+			secsText = 'секунда'
+		} else if(s % 10 === 2 || s % 10 === 3 || s % 10 === 4) {
+			secsText = 'секунды'
+		} else {
+			secsText = 'секунд'
+		}
+	}
+	
+	document.getElementById("timer")
+	  .innerHTML =
+		'<div>' + d + '<span>' + daysText + '</span></div>' +
+	  	'<div>' + h + '<span>'+ hoursText + '</span></div>' +
+		'<div>' + m + '<span>'+  minsText +'</span></div>' +
+		'<div>' + s + '<span>'+ secsText +'</span></div>'
+	}, 1000)
+
+
+// function correctName(d) {
+// 	if(d % 10 === 1) {
+// 		daysText = 'день'
+// 	} else if(d % 10 === 2 || d % 10 === 3 || d % 10 === 4) {
+// 		daysText = 'дня'
+// 	} else {
+// 		daysText === 'дней'
+// 	}
+// }
 })
